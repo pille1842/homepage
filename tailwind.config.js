636463f362ee054dbcef5738f0d9b54ee5199b0e
Eric Haberstroh <gpg@erixpage.de>
2022-07-13 16:46:32 +0200

    Add Webpack Encore, TailwindCSS and Tailwind Elements

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html.twig", "./assets/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
+}
